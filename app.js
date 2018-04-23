f8e5c63981135cce86f4b7129ae9743f666e8901
Ryan Coleman <rcoleman@paradowski.com>
2018-04-23 17:13:33 -0500

    Linked with s3 bucket

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
+const AWS = require('aws-sdk')
 
-app.get('/', (req, res) => res.send('Hello, World!'))
+const params = {
+    Bucket: 'elasticbeanstalk-us-east-2-192235711219'
+}
+
+app.get('/', (req, res) => {
+    AWS.S3.listObjects(params, function(err, data) {
+        if (err) res.return(err, err.stack)
+        else res.return(data)
+    })
+})
 
 app.listen(process.env.PORT, () => console.log(`Sasha Fill is now running on port ${process.env.PORT}!`))
