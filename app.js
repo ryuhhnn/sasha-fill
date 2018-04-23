6d4eb65d857052cb2b1949583d84cb65dcb2e6d7
Ryan Coleman <rcoleman@paradowski.com>
2018-04-23 17:39:25 -0500

    Added redirect to the random image

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ const s3 = new AWS.S3({
 let photos = []
 
 app.get('/', (req, res) => {
-    s3.listObjects({ Delimiter: '/photos' }, function (err, data) {
+    s3.listObjects({ Delimiter: '/photos/' }, function (err, data) {
         if (err) res.send(err, err.stack)
         else photos = data.Contents
 
-        res.send(photos[Math.floor(Math.random() * photos.length)])
+        res.redirect(`https://s3.us-east-2.amazonaws.com/elasticbeanstalk-us-east-2-192235711219/photos/${photos[Math.floor(Math.random() * photos.length)].Key}`)
     })
 })
 
