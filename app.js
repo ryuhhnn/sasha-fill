0515833e3107c499eb4979d376a5de4527258bd8
Ryan Coleman <rcoleman@paradowski.com>
2018-04-23 17:24:06 -0500

    Updates to the AWS sdk

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express')
 const app = express()
 const AWS = require('aws-sdk')
 
-const params = {
-    Bucket: 'elasticbeanstalk-us-east-2-192235711219'
-}
+AWS.config.update({
+    region: 'us-east-2'
+})
+
+const s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    params: {
+        Bucket: 'elasticbeanstalk-us-east-2-192235711219'
+    }
+})
 
 app.get('/', (req, res) => {
-    AWS.S3.listObjects(params, function(err, data) {
+    s3.listObjects({}, function (err, data) {
         if (err) res.return(err, err.stack)
         else res.return(data)
     })
